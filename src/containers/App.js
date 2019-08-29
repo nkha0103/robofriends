f581fc55ed86f352a0d744ddb732d1754d891d8e
Kha Nguyễn <nkha0103@gmail.com>
2019-08-29 15:12:12 +0700

    first commit

---
+++ src/containers/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import './App.css';
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: [],
+            searchField: ''
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users').then((response) => {
+            return response.json();
+        }).then((user) => {
+            this.setState({ robots: user });
+        })
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchField: event.target.value })
+    }
+
+    render() {
+        const { searchField, robots } = this.state;
+        const filteredRobots = robots.filter((robots) => {
+            return robots.name.toLowerCase().includes(searchField.toLowerCase());
+        })
+
+        return !robots.length ?
+            <h1>loading . . .</h1> :
+            (
+                <div className='tc'>
+                    <h1 className='f1'>RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <Scroll>
+                        <CardList robots={filteredRobots} />
+                    </Scroll>
+                </div>
+            )
+
+    }
+}
+
+export default App;
